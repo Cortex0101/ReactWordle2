05f269568fa7d7edeb6cdecf9e26b198edebb8dc
Cortex0101 <ldeiruff@gmail.com>
2024-09-19 12:32:43 +0200

    Made first jest test

---
+++ client/src/__mocks__/i18next.js
@@ -0,0 +1,6 @@
+const i18n = {
+    t: (key) => key, // Mock translation function
+    changeLanguage: () => new Promise(() => { }), // Mock async changeLanguage function
+};
+
+export default i18n;
--- client/src/__tests__/TestComponent.test.js
+++ client/src/__tests__/TestComponent.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'; // For expect(...).toBeInTheDocument()
+import '@testing-library/jest-dom';
 import TestComponent from '../components/TestComponent';
 
 describe('TestComponent', () => {
