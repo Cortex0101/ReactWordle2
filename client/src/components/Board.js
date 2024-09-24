e66457443465b96023b84153ed9beee52c306fe4
Cortex0101 <ldeiruff@gmail.com>
2024-09-24 14:44:26 +0200

    Made board controls function

--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,27 +1,47 @@
 // React-related imports
-import React from "react";
+import React, { useContext } from "react";
 import { Container, Row, Col } from "react-bootstrap";
+
+// Import contentx
+import { GameContext } from "../contexts/GameContext";
 
 // Internal styles
 import './Board.css';
 
-const Board = ({wordLength, maxGuesses}) => {
-    return (
-       <Container className="board">
-		{
-			[...Array(maxGuesses)].map((_, i) => (
-				<Row key={i}>
-					{
-						[...Array(wordLength)].map((_, j) => (
-							<Col key={j} className="board-cell">
-							</Col>
-						))
-					}
+const BoardCell = ({ letter, rowIndex, colIndex, active }) => {
+	const { handleCellClick } = useContext(GameContext);
+
+	return (
+		<Col className={`board-cell mx-1 my-1  ${active ? 'board-cell__selected' : ''}`} onClick={() => {
+			handleCellClick(rowIndex, colIndex);
+		}}>
+			<div className={`board-cell__content`}>
+				{letter}
+			</div>
+		</Col>
+	);
+}
+
+const Board = ({ wordLength, maxGuesses }) => {
+	const { guesses, currentRow, currentCol } = useContext(GameContext);
+
+	return (
+		<Container className="board">
+			{guesses.map((guess, rowIndex) => (
+				<Row key={rowIndex}>
+					{guess.map((letter, colIndex) => (
+						<BoardCell
+							key={colIndex}
+							letter={letter}
+							rowIndex={rowIndex}
+							colIndex={colIndex}
+							active={rowIndex === currentRow && colIndex === currentCol}
+						/>
+					))}
 				</Row>
-			))
-		}
-       </Container>
-    );
+			))}
+		</Container>
+	);
 };
 
 export default Board;
--- client/src/contexts/GameContext.js
+++ client/src/contexts/GameContext.js
@@ -4,7 +4,13 @@ import React, { useState, createContext } from "react";
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
-    const [board, setBoard] = useState([
+    const keyboardLayout = [
+        ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Å'],
+        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Æ', 'Ø'],
+        ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Backspace']
+    ];
+
+    const [guesses, setGuesses] = useState([
         ['', '', '', '', ''],
         ['', '', '', '', ''],
         ['', '', '', '', ''],
@@ -13,25 +19,86 @@ const GameProvider = ({ children }) => {
         ['', '', '', '', '']
     ]);
 
-    const keyboardLayout = [
-        ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'Å'],
-        ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Æ', 'Ø'],
-        ['Enter', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'Backspace']
-    ];
+    const [currentRow, setCurrentRow] = useState(0); // Current row (0-5) being inputted on
+    const [currentCol, setCurrentCol] = useState(0); // Current column (0-4) being inputted on
 
-    // Current row (0-5) being inputted on
-    const [currentRow, setCurrentRow] = useState(0);
+    const handleCellClick = (rowIndex, colIndex) => {
+        if (rowIndex === currentRow) {
+            setCurrentCol(colIndex);
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-    // Curren cell (0-4) being inputted on
-    const [currentCell, setCurrentCell] = useState(0);
+    const addCharacter = (letter) => {
+        /* If the currentRow and currentCol are within bounds, update the current guess at the current index. */
+        if (currentRow < 6 && currentCol < 5) {
+            const newGuess = guesses[currentRow].slice();
+            newGuess[currentCol] = letter;
+            const newGuesses = guesses.slice();
+            newGuesses[currentRow] = newGuess;
+            setGuesses(newGuesses);
+
+            // Move to the next column if not at the end of the row otherwise stay at the end of the row
+            if (currentCol < 4) {
+                setCurrentCol(currentCol + 1);
+            }
+        }
+    }
+
+
+    // Delete a letter in the current guess
+    const deleteCharacter = () => {
+        /* If the current guess at the current col index is not empty, delete it 
+        and decrement the current col index if not at the start of the row otherwise stay at the start of the row */
+        if (currentRow < 6 && currentCol < 5) {
+            const deletedLetter = guesses[currentRow][currentCol];
+
+            const newGuess = guesses[currentRow].slice();
+            newGuess[currentCol] = '';
+            const newGuesses = guesses.slice();
+            newGuesses[currentRow] = newGuess;
+            setGuesses(newGuesses);
+
+            if (currentCol > 0 && deletedLetter === '') {
+                setCurrentCol(currentCol - 1);
+            }
+        }
+    };
+
+    const submitGuess = () => {
+        // Move to the next row if not at the last row otherwise stay at the last row
+        if (currentRow < 5) {
+            setCurrentRow(currentRow + 1);
+            setCurrentCol(0);
+
+            // TODO: Submit guess to server
+        }
+    };
+
+    // Handle user typing input (only update current row)
+    const handleInput = (letter) => {
+        if (letter === 'Backspace') {
+            deleteCharacter();
+            return true;
+        } else if (letter === 'Enter') {
+            submitGuess();
+            return true;
+        } else {
+            addCharacter(letter);
+        }
+    };
 
     return (
-        <GameContext.Provider value={{ 
-            board,
-            currentRow,
-            currentCell,
+        <GameContext.Provider value={{
             keyboardLayout,
-         }}>
+            guesses,
+            currentRow,
+            currentCol,
+            handleCellClick,
+            handleInput
+        }}>
             {children}
         </GameContext.Provider>
     );
