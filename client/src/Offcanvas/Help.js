d168bf793f57ce4ad8ac91d2812c68fb0e05c6d1
Cortex0101 <ldeiruff@gmail.com>
2024-09-26 13:13:58 +0200

    Board func 2

--- client/src/Offcanvas/Help.js
+++ client/src/Offcanvas/Help.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useContext } from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { useTranslation } from "react-i18next";
 
@@ -9,24 +10,100 @@ import { MenuContext } from "../contexts/MenuContext";
 import "./Help.css"
 
 const Help = () => {
-    const { t } = useTranslation(); // Correct usage of the hook
+  const { t } = useTranslation(); // Correct usage of the hook
 
-    const toggleHelpOpen = useContext(MenuContext).toggleHelpOpen;
-    const helpOpen = useContext(MenuContext).helpOpen;
+  const toggleHelpOpen = useContext(MenuContext).toggleHelpOpen;
+  const helpOpen = useContext(MenuContext).helpOpen;
 
-    return (
-        <Modal show={helpOpen} onHide={toggleHelpOpen}>
-            <Modal.Header closeButton>
-                <Modal.Title>{t('modals.help.title')}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-            <Modal.Footer>
-                <Button variant="secondary" onClick={toggleHelpOpen}>
-                    Close
-                </Button>
-            </Modal.Footer>
-        </Modal>
-    );
+  return (
+    <Modal show={helpOpen} onHide={toggleHelpOpen}>
+      <Modal.Header closeButton>
+        <Modal.Title>{t('modals.help.title')}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h3>{t('modals.help.content.rules.title')}</h3>
+
+        <ul>
+          <li>{t('modals.help.content.rules.ul.li1')}</li>
+          <li>{t('modals.help.content.rules.ul.li2')}</li>
+          <li>{t('modals.help.content.rules.ul.li3')}</li>
+        </ul>
+
+        <h3>{t('modals.help.content.examples.title')}</h3>
+
+        <Container>
+          <Row>
+            <Col xs={6} md={3}>
+              <Row>
+                <Col className="board-cell">
+                  <div className={`board-cell__content`}>
+                    S
+                  </div>
+                </Col>
+                <Col className="board-cell">
+                  <div className={`board-cell__content`}>
+                    S
+                  </div>
+                </Col>
+                <Col className="board-cell">
+                  <div className={`board-cell__content`}>
+                    S
+                  </div>
+                </Col>
+                <Col className="board-cell">
+                  <div className={`board-cell__content`}>
+                    S
+                  </div>
+                </Col>
+                <Col className="board-cell">
+                  <div className={`board-cell__content`}>
+                    S
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                DOWN
+              </Row>
+            </Col>
+            <Col xs={6} md={3}>
+              <p>test</p>
+            </Col>
+            <Col xs={6} md={3}>
+              test
+            </Col>
+            <Col xs={6} md={3}>
+              test
+            </Col>
+          </Row>
+        </Container>
+
+        <ul>
+          <li>{t('modals.help.content.examples.ul.li1')}</li>
+          <li>{t('modals.help.content.examples.ul.li2')}</li>
+          <li>{t('modals.help.content.examples.ul.li3')}</li>
+        </ul>
+
+        <h3>{t('modals.help.content.explanation.title')}</h3>
+
+        <ul>
+          <li>
+            <h4>{t('modals.help.content.explanation.ul.li1.title')}</h4>
+            <p>{t('modals.help.content.explanation.ul.li1.content')}</p>
+          </li>
+          <li>
+            <h4>{t('modals.help.content.explanation.ul.li2.title')}</h4>
+            <p>{t('modals.help.content.explanation.ul.li2.content')}</p>
+          </li>
+        </ul>
+
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={toggleHelpOpen}>
+          {t('modals.help.close')}
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default Help;
--- client/src/components/Board.js
+++ client/src/components/Board.js
@@ -1,5 +1,5 @@
 // React-related imports
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 
 // Import contentx
@@ -11,8 +11,20 @@ import './Board.css';
 const BoardCell = ({ letter, rowIndex, colIndex, active }) => {
 	const { handleCellClick } = useContext(GameContext);
 
+	const [isAnimating, setIsAnimating] = useState(false);
+
+	useEffect(() => {
+		if (letter !== '') {
+			setIsAnimating(true);
+			const timer = setTimeout(() => setIsAnimating(false), 300); // Animation duration
+
+			return () => clearTimeout(timer); // Clean up the timeout if component unmounts
+		}
+	}, [letter]); // Trigger the effect when the letter changes
+
+
 	return (
-		<Col className={`board-cell mx-1 my-1  ${active ? 'board-cell__selected' : ''}`} onClick={() => {
+		<Col className={`board-cell mx-1 my-1  ${active ? 'board-cell__selected' : ''} ${isAnimating ? 'letter-added' : ''}`} onClick={() => {
 			handleCellClick(rowIndex, colIndex);
 		}}>
 			<div className={`board-cell__content`}>
@@ -26,21 +38,23 @@ const Board = ({ wordLength, maxGuesses }) => {
 	const { guesses, currentRow, currentCol } = useContext(GameContext);
 
 	return (
-		<Container className="board">
-			{guesses.map((guess, rowIndex) => (
-				<Row key={rowIndex}>
-					{guess.map((letter, colIndex) => (
-						<BoardCell
-							key={colIndex}
-							letter={letter}
-							rowIndex={rowIndex}
-							colIndex={colIndex}
-							active={rowIndex === currentRow && colIndex === currentCol}
-						/>
-					))}
-				</Row>
-			))}
-		</Container>
+		<div className="board-container">
+			<Container className="board">
+				{guesses.map((guess, rowIndex) => (
+					<Row key={rowIndex}>
+						{guess.map((letter, colIndex) => (
+							<BoardCell
+								key={colIndex}
+								letter={letter}
+								rowIndex={rowIndex}
+								colIndex={colIndex}
+								active={rowIndex === currentRow && colIndex === currentCol}
+							/>
+						))}
+					</Row>
+				))}
+			</Container>
+		</div>
 	);
 };
 
